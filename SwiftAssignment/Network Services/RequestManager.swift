da37fe10adfeb33170f13458fce8c8d9f34f739c
Chetan Girase <chetan.girase@deskera.org>
2020-04-17 20:11:04 +0530

    Removed Post method from Request Manager

--- SwiftAssignment/Network Services/RequestManager.swift
+++ SwiftAssignment/Network Services/RequestManager.swift
@@ -34,22 +34,4 @@ class RequestManager {
             }.resume()
         }
     }
-
-    func withPost(apiConfiguration: APIConfiguration, completionHandler: @escaping JSONCompletionHandler) {
-        let configuration = apiConfiguration.configuration()
-        let session = URLSession(configuration: configuration)
-        if let urlRequest = apiConfiguration.postURLRequest() {
-            session.dataTask(with: urlRequest) { (data, _, error) in
-                if let data = data {
-                    if let json = String(data: data, encoding: String.Encoding.utf8) {
-                        completionHandler(json, nil)
-                    } else {
-                        completionHandler(nil, error)
-                    }
-                } else {
-                    completionHandler(nil, error)
-                }
-            }.resume()
-        }
-    }
 }
